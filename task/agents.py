0e5d9b6fa3822503a04adb2cc5416210586ce627
Bayden Ibrahim <baydenibrahim@gmail.com>
2023-04-26 13:59:35 -0400

    modified value_iteration function and tried to implement get_successors to explore states that are known successors

--- task/agents.py
+++ task/agents.py
@@ -136,53 +136,73 @@ class MarkovModel():
 
 
     def value_iteration(self):
-
-        # starting at 0, do value iteration for k times, default 100
-        for i in range(self.k):
+        # Repeat for k iterations or until convergence
+        for state_index in range(self.k):
+            # Initialize update difference and updated values
             update_diff = 0 
-            updated_values = [0]*len(self.states)  # Initialize values
-
-            # go through every state, using index because states, rewards, and policy use the same index 
-            # to represent the same state
-            for i in range(len(self.states)):
+            updated_values = [0] * len(self.states) 
+            
+            # For each state in the state space
+            for next_state_index in range(len(self.states)):
+                # Get the state and set the maximum value to zero
+                s = self.states[next_state_index]
                 max_val = 0
-                s = self.states[i]
-                for a in self.actions:
-
+                
+                # For each possible action in the action space
+                for action in self.actions:
                     # Compute the state value
-                    val = self.rewards[i]  # Get reward value of state
+                    val = self.rewards[next_state_index]  # Get reward value of state
                     
-                    # go through all states again
-                    # could be improved by only going through states that are known successors
-                    for j in range(len(self.states)):
-                        s_next = self.states[j]
-                        # if there is a non zero probability
-                        if (s, s_next, a) in self.transitions:
+                    # For each possible next state in the state space
+                    '''IF ONLY GOING THROUGH SUCCESSORS, USE BELOW?
+                    for s_next in self.get_successors(self, self.states, self.transitions)'''
+                    for num_iterations in range(len(self.states)):
+                        s_next = self.states[num_iterations]
+                        # Check if there is a non-zero probability
+                        if (s, s_next, action) in self.transitions:
                             # Add the product of the probability and the discounted value of the successor
-                            val += self.transitions[(s, s_next, a)] * (self.discount * self.values[j])
-
-                    # Store best value so far
+                            val += self.transitions[(s, s_next, action)] * (self.discount * self.values[num_iterations])
+                    
+                    # Update policy if the value is greater than the maximum value so far
                     if val > max_val:
                         max_val = val
-                        self.policies[i] = a
-
-                    # Update policy based on new values
-                    if self.values[i] < val:
-                        # Store action with highest value
-                        self.policies[i] = a
-
-                updated_values[i] = max_val 
-
-                # Update the max difference to know if we've reached delta (there's virtually no change)
-                update_diff = max(update_diff, abs(self.values[i] - updated_values[i]))
-
-            # Update value functions
+                        self.policies[next_state_index] = action
+                    
+                    # Update the value if the new value is greater than the current value
+                    if self.values[next_state_index] < val:
+                        self.values[next_state_index] = val
+                        updated_values[next_state_index] = val
+                
+                # Compute the difference between the updated and current value for the current state
+                update_diff = max(update_diff, abs(self.values[next_state_index] - updated_values[next_state_index]))
+            
+            # Update the value function with the updated values
             self.values = updated_values
-
-            # convergence
+            
+            # Check if convergence has been reached
             if update_diff < self.delta:
                 break
 
+    # state is the current state for which the successors are to be generated
+    # transition_probs is the dictionary of transition probabilities
+    
+    '''
+    function iterates over all the available actions and computes the probability distribution over the next states for each action by querying the transition probabilities dictionary. 
+    the get method of the dictionary is used to retrieve the probability of transitioning from state to next_state given action. 
+    if the (state, action, next_state) key is not present in the dictionary, the probability is assumed to be 0.
+    the function then generates a successor state for each action by randomly sampling from the probability distribution computed for that action using NumPy's np.random.choice method.
+    the result is a list of successor states that can be reached from state by taking any of the available actions.
+    
+    
+    doesnt follow correct format of transition probabilities dictionary sigh
+    '''
+    def get_successors(self, state, transition_probs):
+        successors = []
+        for action in range(self.actions):
+            probs = [transition_probs.get((state, action, next_state), 0) for next_state in range(self.states)]
+            next_state = np.random.choice(self.states, p=probs)
+            successors.append(next_state)
+        return successors
 
     def policy_iteration(self):
         NotImplementedError()
